7bd615e6f181e3863c8fe2d5f1ca0b54685198e4
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-13 22:06:57 +0530

    Add files via upload

---
+++ PubgDriver.java
@@ -0,0 +1,16 @@
+package PrinciplesOfOops;
+
+public class PubgDriver {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Pubg p1 = new PubgV2();
+		p1.fire();
+		p1.teasure();
+		p1.turnLeft();
+		p1.moveForward(5);
+		
+	}
+
+}
