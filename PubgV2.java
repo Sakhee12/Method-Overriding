2ad9e08928a7c3f5b947f2cefba012e86622a2fc
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-13 22:06:42 +0530

    Add files via upload

---
+++ PubgV2.java
@@ -0,0 +1,14 @@
+package PrinciplesOfOops;
+
+public class PubgV2 extends Pubg 
+{
+	public void moveForward(int stpes)
+	{
+		System.out.println("Move Forward");
+	}
+	
+	public void turnLeft()
+	{
+		System.out.println("Turned Left");
+	}
+}
